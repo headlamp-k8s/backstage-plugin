df0e0785b565e313d7d3930eba7d18a29804065e
yolossn <sannagaraj@microsoft.com>
2025-03-28 17:01:08 +0530

    wip

--- headlamp-backend/src/service/router.ts
+++ headlamp-backend/src/service/router.ts
@@ -12,6 +12,8 @@ import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 import { BackstageCredentials } from "@backstage/backend-plugin-api";
 import { ObjectsByEntityRequest } from "@backstage/plugin-kubernetes-backend";
 import { KubernetesRequestAuth } from "@backstage/plugin-kubernetes-common";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
 
 export interface RouterOptions {
   logger: LoggerService;
@@ -99,8 +101,126 @@ export async function createRouter(
   const middleware = MiddlewareFactory.create({ logger, config });
 
   router.use(middleware.error());
+
+  // List of all static asset paths that headlamp serves
+  const staticAssetPaths = [
+    '/',
+    '/assets',
+    '/android-chrome',
+    '/apple-touch-icon',
+    '/favicon',
+    '/icon',
+    '/logo',
+    '/mstile',
+    '/safari-pinned-tab',
+    '/manifest.json',
+    '/robots.txt',
+    '/mockServiceWorker.js',
+    '/index.html'
+  ];
+
+  function authenticateRequest(req,res,next){
+
+    console.log(`Authenticating request: ${req.path}`);
+    // Skip authentication for static assets
+    if (staticAssetPaths.some(assetPath => req.path.startsWith(assetPath)))  {
+      next();
+      return;
+    }
+
+    const token = req.headers['X-Backstage-Token'];
+    if (!token) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    // TODO: Update authentication logic
+    if (token !== 'backstage-token') {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    next();
+  }
+
+  const proxy = createProxyMiddleware({
+    target: "http://localhost:4466",
+    ws: true,
+    secure: false,
+    changeOrigin: true,
+    logLevel: "debug",
+    pathRewrite: (path: string) => {
+      // if path is static asset, return path
+      logger.info(`===== Rewriting path: ${path}`);
+      return path;
+      // const staticPathMatch = staticAssetPaths.find(p => path.startsWith(`/api/headlamp${p}`));
+      // logger.info(`===== Rewriting path: ${path}  ${staticPathMatch}`);
+      // if (staticPathMatch) {
+      //   logger.info(`===== Returning path: ${path}`);
+      //   return path;
+      // }
+      // // if path is not static asset, return /api/headlamp${path}
+      // logger.info(`===== Not matching static asset path: Returning path: /api/headlamp${path}`);
+      // return `/api/headlamp${path}`;
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      logger.info(`Proxying request to Headlamp: ${req.method} ${req.url}`);
+      // proxyReq.setHeader('Origin', 'http://localhost:4466');
+
+      const origin = req.headers.origin;
+      if (origin) {
+        proxyReq.setHeader('Origin', origin);
+      }
+
+      if (req.headers.referer) {
+        proxyReq.setHeader('Referer', req.headers.referer);
+      }
+
+      if(req.headers.authorization) {
+        proxyReq.setHeader('Authorization', req.headers.authorization);
+      }
+
+      // handle options request
+      if (req.method === 'OPTIONS') {
+        proxyReq.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        proxyReq.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.status(204).end();
+      }
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      logger.info(`Received response from Headlamp: ${proxyRes.statusCode}`);
+      res.setHeader(
+        'Content-Security-Policy',
+        `script-src 'self' 'unsafe-inline' 'unsafe-eval';`
+      );
+
+      proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.origin || '*';
+      proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH, OPTIONS';
+      proxyRes.headers['Access-Control-Allow-Headers'] = 'X-HEADLAMP_BACKEND-TOKEN, X-Requested-With, Content-Type, Authorization, Forward-To, KUBECONFIG, X-HEADLAMP-USER-ID';
+      // const origin = req.headers.origin;
+      // if (origin) {
+      //   res.setHeader('Access-Control-Allow-Origin', origin);
+      //   res.setHeader('Access-Control-Allow-Credentials', 'true');
+      // }else{
+      //   res.setHeader('Access-Control-Allow-Origin', '*');
+      // }
+      // if (req.method === 'OPTIONS') {
+      //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      //   res.status(204).end();
+      // }
+    },
+    onError: (err, req, res) => {
+      logger.error(`Error proxying request: ${err}`);
+      res.status(500).json({ message: "Error proxying request" });
+    }
+  });
+
+  router.use("/", authenticateRequest, proxy);
+
   return router;
 }
+
 
 async function spawnHeadlamp(
   logger: LoggerService,
@@ -122,6 +242,8 @@ async function spawnHeadlamp(
     kubeconfigPath,
     "--plugins-dir",
     pluginsPath,
+    "--base-url",
+    "/api/headlamp"
   ]);
 
   headlampProcess.stdout.on("data", (data) => {
